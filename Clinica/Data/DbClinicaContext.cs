0f628630d7a7f5b0a1f131e2f5a1d1f5da7ca569
samuelerm08 <samuelrm08@gmail.com>
2023-01-27 16:08:50 -0300

    Add project files.

---
+++ Clinica/Data/DbClinicaContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Clinica.Data
+{
+    public class DbClinicaContext : DbContext
+    {
+        public DbClinicaContext(DbContextOptions<DbClinicaContext> options) : base (options) { }        
+    }
+}
---
+++ Clinica/Models/Medico.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Clinica.Models
+{
+    public class Medico
+    {
+        public int MedicoId { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required]
+        [StringLength(50)]
+        public string Apellido { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required]
+        [RegularExpression(@"^[A-Z]{2}[0-9]{4}$")]
+        public string Matricula { get; set; }
+    }
+}
