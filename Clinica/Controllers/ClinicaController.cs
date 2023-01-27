c57b4d8797917d2f4816bbbb9191aaebb4392274
samuelerm08 <samuelrm08@gmail.com>
2023-01-27 16:11:46 -0300

    initial-commit

---
+++ Clinica/Controllers/ClinicaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clinica.Controllers
+{
+    public class ClinicaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
